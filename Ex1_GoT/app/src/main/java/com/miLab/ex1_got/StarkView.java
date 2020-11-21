fb8beb16765e26be2da95ff6554803c0e9173e6c
Chen Sternfeld <chen.ster208@gmail.com>
2020-11-21 14:14:21 +0200

    main is OK

---
+++ Ex1_GoT/app/src/main/java/com/miLab/ex1_got/StarkView.java
@@ -0,0 +1,14 @@
+package com.milab.ex1_got;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StarkView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stark_view);
+    }
+}
