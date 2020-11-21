92cf6f381d51eae06ce434c6f244f554c3cf5417
Chen Sternfeld <chen.ster208@gmail.com>
2020-11-21 12:24:56 +0200

    First GoT commit

---
+++ Ex1_GoT/app/src/main/java/com/miLab/ex1_got/MainActivity.java
@@ -0,0 +1,36 @@
+package com.miLab.ex1_got;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button starkButton = (Button)findViewbyId(R.id.stark);
+        starkButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), StarkActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button lannisterButton = (Button)myView.findViewbyId(R.id.lannister);
+        starkButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), LannisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    
+
+}
---
+++ Ex1_GoT/app/src/main/java/com/miLab/ex1_got/StarkActivity.java
@@ -0,0 +1,25 @@
+package com.miLab.ex1_got;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StarkActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stark);
+
+//        RecyclerView recyclerView = (RecyclerView)findViewByid(R.id.list)
+//        recyclerView.setLayoutManager(new LinearLayoutManager((this)));
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
