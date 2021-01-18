5efd43dc564c657a96ce59056f232328322817e7
Chen Sternfeld <chen.ster208@gmail.com>
2021-01-18 18:06:33 +0200

    progressing with main

--- Ex6_StockMonitoring/app/src/main/java/com/milab/stock_app/MainActivity.java
+++ Ex6_StockMonitoring/app/src/main/java/com/milab/stock_app/MainActivity.java
@@ -7,15 +7,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-//import
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.installations.FirebaseInstallations;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -25,7 +34,10 @@ import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MY_SERVER = "http://192.168.1.15:3000/";
     private static final String TAG= "myTAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,31 +45,44 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView myText = (TextView) findViewById(R.id.textView);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://www.google.com";
-        String myURL = "http://192.168.1.15:3000/";
-
-//        StringRequest stringReq = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-        StringRequest stringReq = new StringRequest(Request.Method.GET, myURL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-//                textView.setText("Response is: "+ response.substring(0,500));
-                Log.d(TAG, response.substring(0, 50));
-                myText.setText("You got it!!");
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "didn't work!!!");
-            }
-        });
+//        RequestQueue queue = Volley.newRequestQueue(this);
 
         Button sendButton = (Button)findViewById(R.id.button);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)  {
-                queue.add(stringReq);
+                String stockName = ((EditText) findViewById(R.id.givenSymbol)).getText().toString();
+                handleServerSide(stockName);
             }
         });
+    }
+
+    // initializes firebase connection and sends request to server
+    public void handleServerSide(String stockName){
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this,  new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                String token = instanceIdResult.getToken();
+                try {
+                    getThePrice(stockName, token);
+                } catch (JSONException e) {
+                    Log.e(TAG, "Error while requesting from server");
+                    e.printStackTrace();
+                }
+            }
+        });
+        FirebaseInstallations.getInstance();
+    }
+
+    public void getThePrice (String stockName, String token) throws JSONException{
+        JSONObject jsonReq = new JSONObject();
+        jsonReq.put("token", token);
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, MY_SERVER + "stocks/" + stockName,
+                jsonReq, response -> Log.i(TAG, "Post to server done"),
+                error -> Log.e(TAG, "Error posting to server")
+        );
+        //New request - need to adding it into the request queue
+        FetcherRequestQueue.getInstance(this).getQueue().add(req);
     }
 }
